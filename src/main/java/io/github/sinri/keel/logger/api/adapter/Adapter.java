d756e321aed4cf4193673d8cb2c031d4029113b3
sinri <e.joshua.s.e@gmail.com>
2025-11-11 20:49:54 +0800

    Refactor Adapter interface: remove Closeable inheritance for simplified design.
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

--- src/main/java/io/github/sinri/keel/logger/api/adapter/Adapter.java
+++ src/main/java/io/github/sinri/keel/logger/api/adapter/Adapter.java
@@ -1,21 +1,15 @@
 package io.github.sinri.keel.logger.api.adapter;
 
 import javax.annotation.Nonnull;
-import java.io.Closeable;
-import java.io.IOException;
 
 /**
  * @param <T> the type of log entity
  * @param <R> the type of rendered entity
  * @since 5.0.0
  */
-public interface Adapter<T, R> extends Closeable {
+public interface Adapter<T, R> {
     static <T, R> Adapter<T, R> build(@Nonnull Render<T, R> render, @Nonnull LogWriter<R> writer) {
         return new Adapter<>() {
-            @Override
-            public void close() throws IOException {
-                this.writer().close();
-            }
 
             @Nonnull
             @Override
