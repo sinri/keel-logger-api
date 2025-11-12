61eb5100d7f17cc40592ae9fe701809880db597d
sinri <e.joshua.s.e@gmail.com>
2025-11-12 11:57:08 +0800

    Remove `Logger`, `EmbeddedLogger`, `LoggerFactory`, and related logging constructs to simplify and streamline logging API design.
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

--- src/main/java/io/github/sinri/keel/logger/api/factory/StdoutRecorderFactory.java
+++ src/main/java/io/github/sinri/keel/logger/api/factory/StdoutRecorderFactory.java
@@ -1,21 +1,5 @@
 package io.github.sinri.keel.logger.api.factory;
 
-import io.github.sinri.keel.logger.api.event.EventRecorder;
-import io.github.sinri.keel.logger.api.event.Logger;
-
-import javax.annotation.Nonnull;
-
-public interface StdoutRecorderFactory extends RecorderFactory<String>, LoggerFactory {
-    @Nonnull
-    @Override
-    default Logger createLogger(@Nonnull String topic) {
-        return Logger.embedded(topic);
-    }
-
-    @Override
-    default EventRecorder<String> createEventLogRecorder(@Nonnull String topic) {
-        return createLogger(topic);
-    }
-
+public interface StdoutRecorderFactory extends RecorderFactory<String> {
 
 }
