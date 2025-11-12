3bc1588361e74af78639178668f74412fd9fb977
sinri <e.joshua.s.e@gmail.com>
2025-11-12 19:57:09 +0800

    Refactor logging API: consolidate `EventRecorderFactory` and `IssueRecorderFactory` into `RecorderFactory`, introduce `sharedTopicRecordConsumer` for improved modularity and consistency, and update `BaseRecorderFactory` implementation accordingly.
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

--- src/main/java/io/github/sinri/keel/logger/api/factory/RecorderFactory.java
+++ src/main/java/io/github/sinri/keel/logger/api/factory/RecorderFactory.java
@@ -1,4 +1,18 @@
 package io.github.sinri.keel.logger.api.factory;
 
-public interface RecorderFactory extends EventRecorderFactory, IssueRecorderFactory {
+import io.github.sinri.keel.logger.api.consumer.TopicRecordConsumer;
+import io.github.sinri.keel.logger.api.event.EventRecorder;
+import io.github.sinri.keel.logger.api.issue.IssueRecord;
+import io.github.sinri.keel.logger.api.issue.IssueRecorder;
+
+import javax.annotation.Nonnull;
+import java.util.function.Supplier;
+
+public interface RecorderFactory {
+    TopicRecordConsumer sharedTopicRecordConsumer();
+
+    EventRecorder createEventRecorder(@Nonnull String topic);
+
+    <L extends IssueRecord<L>> IssueRecorder<L> createIssueRecorder(@Nonnull String topic, @Nonnull Supplier<L> issueRecordSupplier);
+
 }
--- src/main/java/io/github/sinri/keel/logger/base/factory/BaseRecorderFactory.java
+++ src/main/java/io/github/sinri/keel/logger/base/factory/BaseRecorderFactory.java
@@ -1,9 +1,11 @@
 package io.github.sinri.keel.logger.base.factory;
 
+import io.github.sinri.keel.logger.api.consumer.TopicRecordConsumer;
 import io.github.sinri.keel.logger.api.event.EventRecorder;
 import io.github.sinri.keel.logger.api.factory.RecorderFactory;
 import io.github.sinri.keel.logger.api.issue.IssueRecord;
 import io.github.sinri.keel.logger.api.issue.IssueRecorder;
+import io.github.sinri.keel.logger.base.consumer.BaseTopicRecordConsumer;
 import io.github.sinri.keel.logger.base.event.BaseEventRecorder;
 import io.github.sinri.keel.logger.base.issue.BaseIssueRecorder;
 
@@ -11,13 +13,20 @@ import javax.annotation.Nonnull;
 import java.util.function.Supplier;
 
 public class BaseRecorderFactory implements RecorderFactory {
+    private final TopicRecordConsumer sharedTopicRecordConsumer = new BaseTopicRecordConsumer();
+
+    @Override
+    public TopicRecordConsumer sharedTopicRecordConsumer() {
+        return sharedTopicRecordConsumer;
+    }
+
     @Override
     public EventRecorder createEventRecorder(@Nonnull String topic) {
-        return new BaseEventRecorder(topic);
+        return new BaseEventRecorder(topic, sharedTopicRecordConsumer());
     }
 
     @Override
     public <L extends IssueRecord<L>> IssueRecorder<L> createIssueRecorder(@Nonnull String topic, @Nonnull Supplier<L> issueRecordSupplier) {
-        return new BaseIssueRecorder<>(topic, issueRecordSupplier);
+        return new BaseIssueRecorder<>(topic, issueRecordSupplier, sharedTopicRecordConsumer());
     }
 }
