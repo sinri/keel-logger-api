b5013ee18a9d9f3f4e228b5ed59fd6a7ea02ba4d
sinri <e.joshua.s.e@gmail.com>
2025-12-05 14:49:44 +0800

    Bump `pom.xml` version to `5.0.0-rc.19`. Extract `LogTextRender` interface from `BaseLogWriter` for better separation of concerns and reuse. Remove redundant methods from `BaseLogWriter`.
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

--- src/main/java/io/github/sinri/keel/logger/api/adapter/BaseLogWriter.java
+++ src/main/java/io/github/sinri/keel/logger/api/adapter/BaseLogWriter.java
@@ -3,13 +3,6 @@ package io.github.sinri.keel.logger.api.adapter;
 import io.github.sinri.keel.logger.api.log.SpecificLog;
 import org.jetbrains.annotations.NotNull;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 /**
  * 基本日志接入器。
  * <p>
@@ -19,9 +12,8 @@ import java.util.stream.Collectors;
  *
  * @since 5.0.0
  */
-public class BaseLogWriter implements LogWriterAdapter {
+public class BaseLogWriter implements LogWriterAdapter, LogTextRender {
     private static final BaseLogWriter instance = new BaseLogWriter();
-    private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss XXX");
 
     /**
      * 构造函数。
@@ -45,87 +37,6 @@ public class BaseLogWriter implements LogWriterAdapter {
     @Override
     public void accept(@NotNull String topic, @NotNull SpecificLog<?> log) {
         write(render(topic, log));
-    }
-
-    /**
-     * 渲染日志的分类信息为字符串。
-     *
-     * @param classification 日志的分类信息
-     * @return 渲染后的分类信息字符串
-     */
-    @NotNull
-    protected String renderClassification(@NotNull List<String> classification) {
-        return String.join(",", classification);
-    }
-
-    /**
-     * 渲染日志的异常信息为字符串。
-     *
-     * @param throwable 日志对应的异常对象
-     * @return 渲染后的异常信息字符串
-     */
-    @NotNull
-    protected String renderThrowable(@NotNull Throwable throwable) {
-        return ThrowableRender.renderThrowableChain(throwable);
-    }
-
-    /**
-     * 渲染日志的上下文信息为字符串。
-     *
-     * @param context 日志的上下文信息
-     * @return 渲染后的上下文信息字符串
-     */
-    @NotNull
-    protected String renderContext(@NotNull Map<String, Object> context) {
-        return context.entrySet().stream()
-                      .map(entry -> "\t%s:\t%s".formatted(entry.getKey(), entry.getValue()))
-                      .collect(Collectors.joining("\n"));
-    }
-
-    /**
-     * 渲染日志为字符串。
-     *
-     * @param topic 主题
-     * @param log   日志
-     * @return 日志经渲染后的字符串
-     */
-    @NotNull
-    protected String render(@NotNull String topic, @NotNull SpecificLog<?> log) {
-        StringBuilder sb = new StringBuilder();
-        var zonedDateTime = Instant.ofEpochMilli(log.timestamp()).atZone(ZoneId.systemDefault());
-        sb.append("* ")
-          .append(zonedDateTime.format(formatter)).append(" ")
-          .append("[").append(log.level().name()).append("] ")
-          .append(topic)
-          .append(" <").append(log.threadInfo()).append(">");
-
-        List<String> classification = log.classification();
-        if (classification != null && !classification.isEmpty()) {
-            sb.append("\n").append(renderClassification(classification));
-        }
-
-        String message = log.message();
-        if (message != null && !message.isBlank()) {
-            sb.append("\n").append(message);
-        }
-        Throwable exception = log.exception();
-        if (exception != null) {
-            sb.append("\n")
-              .append(renderThrowable(exception));
-        }
-        Map<String, Object> map = log.context().toMap();
-        if (!map.isEmpty()) {
-            sb.append("\n")
-              .append(renderContext(map));
-        }
-
-        Map<String, Object> extra = log.extra();
-        if (!extra.isEmpty()) {
-            sb.append("\nExtra as following:\n")
-              .append(renderContext(extra));
-        }
-
-        return sb.toString();
     }
 
     /**
