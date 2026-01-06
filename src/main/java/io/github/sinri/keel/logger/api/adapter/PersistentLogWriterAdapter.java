28b38eb086182542d14a469eeae9f55556e9f633
sinri <e.joshua.s.e@gmail.com>
2026-01-06 14:28:59 +0800

    Bump version to `5.0.0-rc.24` in `gradle.properties`. Mark `PersistentLogWriterAdapter` as deprecated for removal in `5.0.0` and update interface to remove `Closeable`.
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

--- src/main/java/io/github/sinri/keel/logger/api/adapter/PersistentLogWriterAdapter.java
+++ src/main/java/io/github/sinri/keel/logger/api/adapter/PersistentLogWriterAdapter.java
@@ -2,14 +2,12 @@ package io.github.sinri.keel.logger.api.adapter;
 
 import org.jspecify.annotations.NullMarked;
 
-import java.io.Closeable;
-
 /**
  * 持久性日志写入适配器。
  *
- * @see Closeable
  * @since 5.0.0
  */
+@Deprecated(since = "5.0.0", forRemoval = true)
 @NullMarked
-public interface PersistentLogWriterAdapter extends Closeable, LogWriterAdapter {
+public interface PersistentLogWriterAdapter extends LogWriterAdapter {
 }
